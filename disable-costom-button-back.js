3ec2d7b0d73a9e1708d941726826bb7f3ab0efa2
oonbeh <oonbeh@gmail.com>
2022-06-22 21:17:13 +0800

    Update disable-costom-button-back.js

--- disable-costom-button-back.js
+++ disable-costom-button-back.js
@@ -2,9 +2,6 @@
 //-- direct link adsterra
 const targetDirectDisableBack = "https://readydolphinpoverty.com/uijctn538c?key=4f219c1087bce980ac7a203a864c9d2b";
 
-//-- direct link propeller ads
-//const targetDirectDisableBack = "//dooloust.net/4/5089450";
-
 history.pushState(null, document.title, window.location.href);
 window.addEventListener('popstate', function (event)
 {
