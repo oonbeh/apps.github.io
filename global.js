259b0eeb7f58bd8207f4e87c63e538d16713f199
oonbeh <oonbeh@gmail.com>
2022-06-23 15:33:10 +0800

    Update global.js

--- global.js
+++ global.js
@@ -3,11 +3,6 @@ const configAds = [
 		"src":"readydolphinpoverty.com/4a/00/1e/4a001e886418d57f6154e262bac9b581.js",
 		"name-element":"script",
 		"target-selector":"body"
-	},
-	{
-		"src":"readydolphinpoverty.com/f5/8d/92/f58d9219ab390554e2e7fc8aeaf82185.js",
-		"name-element":"script",
-		"target-selector":"body"
 	}
 ];
 const initAds = async(config)=>{
